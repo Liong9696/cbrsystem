a57cad07d270c29ddf42e49b4716793ce152fd48
Liong9696 <66987098+Liong9696@users.noreply.github.com>
2020-06-16 21:07:26 +0800

    Update dbs.php

--- dbs.php
+++ dbs.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("localhost","root","","dbphpsearch");
+$con = mysqli_connect("db4free.net","liong_456","liong_456","dbphpsearch");
 // Check connection
 if (mysqli_connect_errno())
   {
