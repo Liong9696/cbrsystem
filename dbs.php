5c1bf30003455c15918cf352e09962f24f075c84
Liong9696 <66987098+Liong9696@users.noreply.github.com>
2020-06-17 13:55:04 +0800

    Update dbs.php

--- dbs.php
+++ dbs.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("db4free.net","liong_456","liong_456","dbphpsearch");
+$con = mysqli_connect("ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","n4rtw9oyycy54r3n","kw96kfvkw3ae94vy","nyrcls6o28ze1y39");
 // Check connection
 if (mysqli_connect_errno())
   {
