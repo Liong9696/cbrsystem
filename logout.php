92d585b441d4ee1305015e2b83eacc6a53396f47
Liong9696 <66987098+Liong9696@users.noreply.github.com>
2020-06-17 14:14:26 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -5,7 +5,7 @@ if(session_destroy())
 {
 // Redirecting To Home Page
 echo "<script type='text/javascript'>alert('You are logged out!')</script>";
-header('refresh:0; url=https://cbrsystem.com/index.php');
+header('refresh:0; url=http://cbrsystem.com/index.php');
 }
 ?>
 
